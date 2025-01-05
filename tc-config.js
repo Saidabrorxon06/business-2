aacc4b1e4f57b81dd5a00dc86fd773b180ac67f6
Saidabrorxon06 <saidabrorahmadov@icloud.com>
2025-01-05 05:29:20 +0500

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://L8yJ9BrknYNh62rfYIvsXBPmsDH9fIaf_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "AH1-Manager"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;span style=&#34;font-size: 16px;&#34;&gt;A manager is an individual that supervises both activities and people within a given organization. In other terms, it is the person in charge of overseeing things that to get done.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;font-size: 16px;&#34;&gt;The concept or figure of a manager is mostly related to business environments. Nonetheless, the managing function can be extended to different spheres by applying the underlying concept.&amp;nbsp;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;font-size: 16px;&#34;&gt;A manager is someone that has the responsibility of getting things done. He normally manages both people and resources (physical resources or economic resources, among others). He has to plan, organize, execute and control all the activities he has been delegated with by using all available resources to do it effectively. Managers normally have enough authority to require and dispose resources as needed.&lt;/span&gt;&lt;/p&gt;"
+};
